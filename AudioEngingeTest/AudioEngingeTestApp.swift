51c9469bfa259960e30d896a314ac3ceb5d05274
Leonid Liadveikin <fantom2259@gmail.com>
2022-10-09 16:20:35 +0200

    Initial Commit

---
+++ AudioEngingeTest/AudioEngingeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioEngingeTestApp.swift
+//  AudioEngingeTest
+//
+//  Created by Leonid Lyadveykin on 09.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct AudioEngingeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
